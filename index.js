e10c88e8aec4f81859bb0dc5b85f5b8b75bdddc7
mickysemu18 <73254380+mickysemu18@users.noreply.github.com>
2023-07-02 14:04:30 -0700

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(flash());
 mongoose.connect('mongodb+srv://mickysemu18:Hebist18@cluster0.s4vx6.mongodb.net/damera_database?retryWrites=true&w=majority', {useNewUrlParser: true })
 let port = process.env.PORT;
 if (port == null || port == ""){
-    port = 3000;
+    port = 6000;
 }
 app.listen(port, ()=>{
   console.log('App listening... ')
