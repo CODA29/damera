8fc2ae7d253cff2c8f505f32e8a71f9ce9f35356
mickysemu18 <rasdagm@gmail.com>
2023-07-17 14:01:16 -0700

    update

---
+++ public/js/burger.js
@@ -0,0 +1,38 @@
+
+var burger = document.querySelector('.burger');
+var links = document.getElementById("myLinks");
+var burgerMenu = document.querySelector(".burgerMenu");
+var icon = document.querySelector(".icon");
+var i = document.getElementById("i");
+const x = document.createElement("i");
+
+
+burger.addEventListener('click', function(){
+
+    
+    x.className = "fa-solid fa-xmark";
+
+    if(burgerMenu.style.display ==="block"){
+        burgerMenu.style.display ="none";
+        icon.replaceChild(i, x);
+
+    }
+    
+    else{
+        burgerMenu.style.display ="block";
+        icon.replaceChild(x, i);
+       
+
+    }
+})
+
+window.addEventListener('resize', function(){
+    if(window.innerWidth > 750){
+        burgerMenu.style.display ="none";
+        icon.replaceChild(i, x);
+    }
+} )
+
+
+
+
