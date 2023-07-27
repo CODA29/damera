32463a664e5b94ab5ca008cbef8cfb4a5428f7fa
mickysemu18 <rasdagm@gmail.com>
2023-07-26 17:00:12 -0700

    Update storePost.js

--- controllers/storePost.js
+++ controllers/storePost.js
@@ -5,7 +5,7 @@ module.exports = (req,res)=>{
     let image = req.files.image;
     console.log('Image:', image)
     image.mv(
-        path.resolve(__dirname,'..','public','img',image.name), 
+        path.resolve(__dirname, '..', 'public', 'img', image.name), 
         async (error) =>{
         await BlogPost.create({
             ...req.body, 
