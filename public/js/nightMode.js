70b7dc5106ab71a701c3af5c404cb5f17653a405
mickysemu18 <rasdagm@gmail.com>
2023-08-10 18:30:20 -0700

    update

--- public/js/nightMode.js
+++ public/js/nightMode.js
@@ -2,6 +2,8 @@ const modeBtn = document.getElementById('modeBtn');
 const modeLabel = document.getElementById('modeLabel');
 const body = document.body;
 const bodyText = document.getElementById('bodyText');
+const footer = document.querySelector('#foot');
+const commentBox = document.querySelector('#commentBox');
 
 const modeDescription ={
     light:{
@@ -20,22 +22,31 @@ const modeDescription ={
 
 
 modeBtn.innerHTML = '<i class="fa-solid fa-moon"></i>';
-modeLabel.textContent = 'DARK MODE'
+modeLabel.textContent = 'Dark'
 
 modeBtn.addEventListener('click', function(){
 
     if(modeBtn.innerHTML === '<i class="fa-solid fa-moon"></i>'){
-        modeLabel.textContent = 'LIGHT MODE'
-        modeBtn.innerHTML = '<i class="fa-solid fa-sun" style="color: #faa44e"></i>';
+        modeLabel.textContent = 'Light'
+        modeBtn.innerHTML = '<i class="fa-solid fa-sun" style="color: #faa44e;"></i>';
         body.style.background ='#454545'
+        footer.style.background ='#454545'
+        commentBox.style.background ='#454545'
+        commentBox.style.borderColor ='white'
+        commentBox.style.color ='white'
         body.style.color ='white'
        
     }
     else{
-        modeLabel.textContent = 'DARK MODE'
+        modeLabel.textContent = 'Dark'
         modeBtn.innerHTML = '<i class="fa-solid fa-moon"></i>';
-        body.style.backgroundImage ='url(https://global-uploads.webflow.com/60d21862724092349138065e/60d22a662e93a76ff5878b0a_geometry.png)'
-        body.style.color ='#454545'
+        body.style.backgroundImage =''
+        body.style.color =''
+        footer.style.background =''
+        commentBox.style.background ='none'
+        commentBox.style.borderColor =''
+        commentBox.style.color =''
+        
     }
     
 
