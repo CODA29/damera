dc6e6c9560332d0a48f6f908d62c2a55703ef775
mickysemu18 <73254380+CODA29@users.noreply.github.com>
2023-08-17 01:19:45 -0700

    update

--- public/js/nightMode.js
+++ public/js/nightMode.js
@@ -5,21 +5,21 @@ modeBtn.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
   if (document.body.classList.contains('dark-mode')) {
     localStorage.setItem('color-scheme', 'dark');
-    modeBtn.innerHTML = '<i class="fa-solid fa-circle-half-stroke" style="color: white"></i>';
-    modeLabel.textContent = "Dark Mode";
+    modeBtn.innerHTML = '<i class="fa-solid fa-circle-half-stroke fa-rotate-180" style="color: #faa44e">';
+    modeLabel.textContent = "Light Mode";
 
   } else {
     localStorage.setItem('color-scheme', 'light');
-    modeBtn.innerHTML = '<i class="fa-solid fa-circle-half-stroke fa-rotate-180" style="color: #faa44e"></i>';
-    modeLabel.textContent = "Light Mode"
+    modeBtn.innerHTML = '<i class="fa-solid fa-circle-half-stroke" style="color: black"></i>';
+    modeLabel.textContent = "Dark Mode"
   }
 });
 
 const userPreference = localStorage.getItem('color-scheme');
 if (userPreference === 'dark') {
   document.body.classList.add('dark-mode');
-  modeBtn.innerHTML = '<i class="fa-solid fa-circle-half-stroke" style="color: white"></i>';
-  modeLabel.textContent = "Dark Mode"
+  modeBtn.innerHTML = '<i class="fa-solid fa-circle-half-stroke fa-rotate-180" style="color: #faa44e">';
+  modeLabel.textContent = "Light Mode";
 }
 
 
