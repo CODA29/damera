c7f8ae1e8a89ae8aa4ed615db95ab6e012697f4b
mickysemu18 <73254380+CODA29@users.noreply.github.com>
2023-08-20 00:56:38 -0700

    update

--- public/js/burger.js
+++ public/js/burger.js
@@ -10,19 +10,16 @@ const x = document.createElement("i");
 burger.addEventListener('click', function(){
 
     
-    x.className = "fa-solid fa-xmark";
+    x.className = "fa-solid fa-xmark fa-lg";
 
     if(burgerMenu.style.display ==="block"){
         burgerMenu.style.display ="none";
         icon.replaceChild(i, x);
-
     }
     
     else{
         burgerMenu.style.display ="block";
         icon.replaceChild(x, i);
-       
-
     }
 })
 
